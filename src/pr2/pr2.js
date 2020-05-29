e8b5255a00b9eb19d6d7a1485a81389b5dacb8fa
IGOR <igornesan@gmail.com>
2020-05-29 21:46:51 +0300

    final

--- src/pr2/pr2.js
+++ src/pr2/pr2.js
@@ -10,36 +10,59 @@ let min = 1,
 //define UI vars
 
 const game = document.querySelector('#game');
-const minNum = document.querySelector('.minNum');
-const maxNum = document.querySelector('.maxNum');
+let minNum = document.querySelector('.minNum');
+let maxNum = document.querySelector('.maxNum');
 const inputNum = document.querySelector('#guess');
 const msgArea = document.querySelector('#msg');
 const submitBtn = document.querySelector('#submitBtn');
 
-//Add Event Listener
-submitBtn.addEventListener('click', playTheGame);
+//define min and max 
+minNum.textContent = min;
+maxNum.textContent = max;
 
-//Compare the numbers
-function playTheGame() {
+//Listen for play again
+game.addEventListener('mousedown', (e) => {
+    if (e.target.classList.contains('play-again')) {
+        window.location.reload();
+    }
+});
+//Add Event Listener
+submitBtn.addEventListener('mouseup', () => {
+    const number = parseInt(inputNum.value);
     //validate
-    if (isNaN(inputNum.value) || inputNum.value < min || inputNum.value > max) {
-        showMsg('Check your number!', 'red');
+    if (isNaN(number) || number < min || number > max) {
+        showMsg(`Please enter number between ${min} and ${max}`, 'red');
     } else {
-        if (inputNum.value === winningNum) {
-            console.log(inputNum.value);
-            gameOver(`${inputNum} is correct. YOU WIN!`, 'green');
+        if (number === winningNum) {
+            gameOver(true, `${number} is correct. YOU WIN!`);
+        } else {
+            guessesLeft -= 1;
+            if (guessesLeft === 0) {
+                gameOver(false, `You Lost! The winning number was ${winningNum}`);
+            } else {
+                showMsg(`${number} is not correct, you have ${guessesLeft} guesses left`, 'red');
+                inputNum.value = '';
+            }
         }
     }
-}
+});
+
+
+
+
 //show message
 function showMsg(msg, color) {
     msgArea.textContent = msg;
     msgArea.style.color = color;
-}
-//Game over
-function gameOver(msg, color) {
-    submitBtn.value = 'Play Again';
-    inputNum.style.borderColor = color;
-    showMsg(msg, color);
 
 }
+//Game over
+function gameOver(win, msg) {
+    let color = win ? 'green' : 'red';
+    submitBtn.value = 'Play Again';
+    submitBtn.classList.add('play-again');
+    inputNum.disabled = true;
+    inputNum.style.borderColor = color;
+    showMsg(msg, color);
+}
+
